e88e13135e22556c17b858ba25a4ab21e1963590
Tran Tuan <tran.van.tuan@mqsolutions.com.vn>
2020-05-21 17:25:50 +0700

    update

--- index.js
+++ index.js
@@ -2,17 +2,22 @@
 /**
  * VNPay Class implement refer from the VNPAY API v2.0 documentation.
  */
-const moment = require('moment');
+const moment = require('moment-timezone');
 const Utils = require('./src/utils');
 const SimpleSchema = require('simpl-schema');
+const {promisify} = require('util');
+const getIP = promisify(require('external-ip')());
 const PayRequestValidator = require('./src/validators/requests/PayRequest');
 const QueryRequestValidator = require('./src/validators/requests/QueryRequest');
 const RefundRequestValidator = require('./src/validators/requests/RefundRequest');
-const VNP_VERSION_DEFAULT = '2';
+const VNP_VERSION_DEFAULT = '2.0.0';
 const CURRENCY_VND_CODE = 'VND';
 const LOCALE_EN = 'en';
 const LOCALE_VN = 'vn';
-const VNP_URL = 'http://sandbox.vnpayment.vn/paymentv2/vpcpay.html';
+const DEFAUT_CREATEDATE_TIMEZONE = 'Asia/Ho_Chi_Minh';
+const VNP_PAY_URL = 'https://sandbox.vnpayment.vn/paymentv2/vpcpay.html';
+const VNP_REFUND_URL = 'http://sandbox.vnpayment.vn/merchant_webapi/merchant.html';
+const VNP_QUERY_URL = 'http://sandbox.vnpayment.vn/merchant_webapi/merchant.html';
 
 class VNPay {
     constructor (config = {}) {
@@ -21,10 +26,11 @@ class VNPay {
             secretKey: { type: String },
             merchantCode: { type: String, max: 8 },
             returnUrl: { type: String, min: 10, max: 255 },
-            ipAddr: { type: String, min: 7, max: 45 },
+            ipAddr: { type: String, optional: true, min: 7, max: 45 },
             locale: { type: String, optional: true, allowedValues: [LOCALE_EN, LOCALE_VN] },
             vnpVersion: { type: String, optional: true, allowedValues: ['1', VNP_VERSION_DEFAULT] },
-            hashAlgorithm: { type: String, optional: true, allowedValues: ['sha256', 'md5'] }
+            hashAlgorithm: { type: String, optional: true, allowedValues: ['sha256', 'md5'] },
+            timezone: { type: String, optional: true }
         });
         this.setConfig(config);
         this.vnpDefaultParams = {
@@ -45,12 +51,21 @@ class VNPay {
             merchantCode: config.merchantCode,
             returnUrl: config.returnUrl,
             ipAddr: config.ipAddr,
-            hashAlgorithm: config.hashAlgorithm ? config.hashAlgorithm.toLowerCase() : 'sha256'
+            hashAlgorithm: config.hashAlgorithm ? config.hashAlgorithm.toLowerCase() : 'sha256',
+            timezone: config.timezone || DEFAUT_CREATEDATE_TIMEZONE
         };
     }
 
-    pay (payload) {
+    async genPayURL (payload) {
         PayRequestValidator.validate(payload);
+        let merchantIpAddr;
+        if (!this.config.ipAddr) {
+            merchantIpAddr = await this.constructor.getPublicIP()
+        } else {
+            merchantIpAddr = this.config.ipAddr;
+        }
+        let createDate = this.genCreateDate()
+        let expiredDate = createDate + 1500;
         let vnpParams = {
             ...this.vnpDefaultParams,
             vnp_Command: 'pay',
@@ -59,8 +74,11 @@ class VNPay {
             vnp_OrderInfo: payload.orderInfo,
             vnp_OrderType: payload.orderType,
             vnp_Amount: payload.amount * 100,
-            vnp_CreateDate: this.genCreateDate()
+            vnp_CreateDate: createDate,
+            vnp_ExpireDate: expiredDate,
+            vnp_IpAddr: merchantIpAddr
         };
+        
         if (payload.bankCode && payload.bankCode !== '') {
             vnpParams.vnp_BankCode = payload.bankCode;
         }
@@ -70,19 +88,27 @@ class VNPay {
         // return vnpParams;
         let urlParams = Utils.makeUrlParams(vnpParams);
 
-        return `${VNP_URL}?${urlParams}`;
+        return `${VNP_PAY_URL}?${urlParams}`;
     }
 
-    refund (payload) {
-        RefundRequestValidator.validate(payload)
+    async genRefundURL (payload) {
+        RefundRequestValidator.validate(payload);
+        let merchantIpAddr;
+        if (!this.config.ipAddr) {
+            merchantIpAddr = await this.constructor.getPublicIP()
+        } else {
+            merchantIpAddr = this.config.ipAddr;
+        }
         let vnpParams = {
             ...this.vnpDefaultParams,
             vnp_Command: 'refund',
+            vnp_ReturnUrl: this.config.returnUrl,
             vnp_TransactionType: payload.transactionType,
             vnp_TxnRef: payload.transactionRef,
             vnp_Amount: payload.amount * 100,
             vnp_TransDate: payload.transDate,
-            vnp_CreateDate: this.genCreateDate()
+            vnp_CreateDate: this.genCreateDate(),
+            vnp_IpAddr: merchantIpAddr
         }
         if (payload.orderInfo && payload.orderInfo !== '') {
             vnpParams.vnp_OrderInfo = payload.orderInfo;
@@ -93,11 +119,17 @@ class VNPay {
 
         let urlParams = Utils.makeUrlParams(vnpParams);
 
-        return `${VNP_URL}?${urlParams}`;
+        return `${VNP_REFUND_URL}?${urlParams}`;
     }
 
-    queryDr (payload) {
+    async genQueryURL (payload) {
         QueryRequestValidator.validate(payload);
+        let merchantIpAddr;
+        if (!this.config.ipAddr) {
+            merchantIpAddr = await this.constructor.getPublicIP()
+        } else {
+            merchantIpAddr = this.config.ipAddr;
+        }
         let vnpParams = {
             ...this.vnpDefaultParams,
             vnp_Command: 'querydr',
@@ -105,7 +137,8 @@ class VNPay {
             vnp_OrderInfo: payload.orderInfo,
             vnp_TransDate: payload.transDate,
             vnp_OrderType: payload.orderType,
-            vnp_CreateDate: this.genCreateDate()
+            vnp_CreateDate: this.genCreateDate(),
+            vnp_IpAddr: merchantIpAddr
         }
         if (payload.transactionNo) {
             vnpParams.vnp_TransactionNo = payload.transactionNo;
@@ -115,11 +148,27 @@ class VNPay {
             this.config.hashAlgorithm);
         let urlParams = Utils.makeUrlParams(vnpParams);
 
-        return `${VNP_URL}?${urlParams}`;
+        return `${VNP_QUERY_URL}?${urlParams}`;
     }
 
     genCreateDate () {
-        return parseInt(moment().format('YYYYMMDDHHmmss'));
+        return parseInt(moment().tz(this.config.timezone).format('YYYYMMDDHHmmss'));
     }
+    /**
+     * Get the public IP Address of server. Refer https://github.com/sindresorhus/public-ip to set options
+     * @param {Object} options 
+     */
+    static async getPublicIP (options = {}) {
+        return getIP()
+    }
+
+    checkVNPayResponse (data) {
+        return Utils.isValidResponse(data, this.config.secretKey);
+    }
+
+    convertResponseMessage (responseCode, locale = 'vn') {
+        return Utils.getReturnUrlStatus(responseCode, locale || this.config.locale)
+    }
+
 }
 module.exports = VNPay
--- test/test.js
+++ test/test.js
@@ -1,17 +1,21 @@
 const assert = require('assert');
 const defaultConfig = require('./config.json');
 const VNPay = require('../index');
+const extIP = require('external-ip')();
+
 let vnpay = new VNPay({
     secretKey: defaultConfig.secretKey,
     returnUrl: defaultConfig.returnUrl,
     merchantCode: defaultConfig.merchantCode,
-    ipAddr: '192.168.6.52'
-})
-describe('Generate VNPAY URL for API pay', () => {
-    let payURL = vnpay.pay({
-        transactionRef: 'PT20200520103101_004',
+    hashAlgorithm: 'sha256'
+});
+console.log('genCreateDate', vnpay.genCreateDate());
+
+describe('Generate VNPAY URL for API pay', async () => {
+    let payURL = await vnpay.genPayURL({
+        transactionRef: 'PT20200520103101_007',
         orderInfo: 'Thanh toan hoa don dich vu',
-        orderType: '250006',
+        orderType: 'topup',
         amount: 100000,
         bankCode: 'NCB'
     })
@@ -19,10 +23,10 @@ describe('Generate VNPAY URL for API pay', () => {
 })
 console.log('    ')
 
-describe('Generate VNPAY URL for API refund', () => {
-    let refundURL = vnpay.refund({
+describe('Generate VNPAY URL for API refund', async () => {
+    let refundURL = await vnpay.genRefundURL({
         transactionType: '02',
-        transactionRef: 'PT20200520103101_005',
+        transactionRef: 'PT20200520103101_008',
         transDate: 20200520114301,
         orderInfo: 'Tra lai tien KH (kh tra lai hang)',
         amount: 100000
@@ -30,12 +34,32 @@ describe('Generate VNPAY URL for API refund', () => {
     console.log('refundURL', refundURL);
 })
 console.log('    ')
-describe('Generate VNPAY URL for API truy van giao dich', () => {
-    let queryURL = vnpay.queryDr({
-        transactionRef: 'PT20200520103101_AAA',
+describe('Generate VNPAY URL for API truy van giao dich', async () => {
+    let queryURL = await vnpay.genQueryURL({
+        transactionRef: 'PT20200520103101_007',
         orderType: '250006',
         transDate: 20200520104301,
         orderInfo: 'Kiem tra ket qua giao dich',
     })
     console.log('queryURL', queryURL);
+})
+
+describe('Test response', () => {
+    let responseData = {
+        vnp_Amount: 1000000,
+        vnp_BankCode: 'NCB',
+        vnp_BankTranNo: '20200521-162954',
+        vnp_CardType: 'ATM',
+        vnp_OrderInfo: 'Thanh toan don hang thoi gian: 2020-05-21 16:05:42',
+        vnp_PayDate: 20200521162945,
+        vnp_ResponseCode: '00',
+        vnp_TmnCode: '1SNJ89L8',
+        vnp_TransactionNo: '13304053',
+        vnp_TransactionStatus: '00',
+        vnp_TxnRef: '160529',
+        vnp_SecureHashType: 'SHA256',
+        vnp_SecureHash: '8cd9d2efe8d67a39bdc1272540acfe712f610d5e154fa57dc83c6d0854d30c58'
+    };
+    console.log('is Valid response ', vnpay.checkVNPayResponse(responseData))
+    console.log('Message: ' + vnpay.convertResponseMessage(responseData.vnp_ResponseCode))
 })
